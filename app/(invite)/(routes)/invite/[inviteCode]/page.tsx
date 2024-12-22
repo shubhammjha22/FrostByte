1b51983ace2da60e053bd6beff78b4d82b12e711
shubhammjha22 <shubhammjha22@gmail.com>
2024-12-22 09:15:12 +0530

    added invite page

---
+++ app/(invite)/(routes)/invite/[inviteCode]/page.tsx
@@ -0,0 +1,58 @@
+import { currentProfile } from "@/lib/current-profile";
+import prisma from "@/lib/db";
+import { RedirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+interface InviteCodeProps {
+  params: {
+    inviteCode: string;
+  };
+}
+
+const InviteCodePage = async ({ params }: InviteCodeProps) => {
+  const profile = await currentProfile();
+  if (!profile) {
+    return redirect("/");
+  }
+
+  const { inviteCode } = await params;
+  if (!inviteCode) {
+    return redirect("/");
+  }
+
+  const existingServer = await prisma.server.findFirst({
+    where: {
+      inviteCode,
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+
+  if (existingServer) {
+    return redirect(`/servers/${existingServer.id}`);
+  }
+
+  const server = await prisma.server.update({
+    where: {
+      inviteCode,
+    },
+    data: {
+      members: {
+        create: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+
+  if (server) {
+    return redirect(`/server/${server.id}`);
+  }
+
+  return null;
+};
+
+export default InviteCodePage;
