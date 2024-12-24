f916e04e546b7cbd171639780f87f551761cf733
shubhammjha22 <shubhammjha22@gmail.com>
2024-12-24 18:12:06 +0530

    added delete modal 2/2

---
+++ components/modals/delete-server-modal.tsx
@@ -0,0 +1,87 @@
+"use client";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "../ui/dialog";
+
+import { useModal } from "@/hooks/use-modal-store";
+
+import { Button } from "../ui/button";
+import { useState } from "react";
+import axios from "axios";
+import { NextResponse } from "next/server";
+import { useRouter } from "next/navigation";
+
+export default function DeleteServerModal() {
+  const { isOpen, onOpen, onClose, type, data } = useModal();
+
+  const isModalOpen = isOpen && type === "deleteServer";
+  const { server } = data;
+  const router = useRouter();
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onConfirmLeave = async () => {
+    try {
+      setIsLoading(true);
+
+      await axios.delete(`/api/servers/${server?.id}/delete`);
+      onClose();
+      router.refresh();
+      router.push("/");
+    } catch (error) {
+      console.log(error);
+      return new NextResponse("Internal Server error", { status: 500 });
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  function handleClose() {
+    onClose();
+    console.log(isOpen);
+  }
+
+  return (
+    <>
+      {isModalOpen && (
+        <Dialog open={isModalOpen} onOpenChange={handleClose}>
+          <DialogContent className="bg-white text-black p-0 overflow-hidden ">
+            <DialogHeader className="pt-8 px-6">
+              <DialogTitle className="text-2xl text-center font-bold">
+                Leave Server
+              </DialogTitle>
+              <DialogDescription className="text-center text-zinc-500">
+                Are you sure you want to delete this{" "}
+                <span className="font-semibold text-indigo-500">
+                  {" "}
+                  {server?.name}{" "}
+                </span>{" "}
+                ?
+              </DialogDescription>
+            </DialogHeader>
+            <DialogFooter className="bg-gray-100 px-6 py-4">
+              <div className="flex items-center justify-between w-full">
+                <Button disabled={isLoading} onClick={onClose} variant="ghost">
+                  Cancel
+                </Button>
+                <Button
+                  disabled={isLoading}
+                  onClick={onConfirmLeave}
+                  variant="primary"
+                >
+                  Confirm
+                </Button>
+              </div>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
+      )}
+    </>
+  );
+}
