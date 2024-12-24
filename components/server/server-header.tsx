ee38d9195f3f949aa1e1e76053ed1006015e49f7
shubhammjha22 <shubhammjha22@gmail.com>
2024-12-24 13:38:41 +0530

    added leave modal

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -98,7 +98,10 @@ export default function ServerHeader({ server, role }: ServerHeaderProps) {
             </DropdownMenuItem>
           )}
           {!isAdmin && (
-            <DropdownMenuItem className="text-rose-500 py-2 px-3 text-sm cursor-pointer">
+            <DropdownMenuItem onClick={() => (
+              setDropdownOpen(false),
+              onOpen("leaveServer",{server}),
+            )} className="text-rose-500 py-2 px-3 text-sm cursor-pointer">
               Leave Server
               <LogOut className="h-4 w-4 ml-auto" />
             </DropdownMenuItem>
