0a51964a60acf6f4debdc74a505d7f503e10a0fd
shubhammjha22 <shubhammjha22@gmail.com>
2024-12-24 11:04:23 +0530

    added members manage api route

---
+++ app/api/members/[memberId]/route.ts
@@ -0,0 +1,63 @@
+import { currentProfile } from "@/lib/current-profile";
+import prisma from "@/lib/db";
+import { NextResponse } from "next/server";
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { memberId: string } }
+) {
+  try {
+    const profile = await currentProfile();
+    if (!profile) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const { searchParams } = new URL(req.url);
+    const { role } = await req.json();
+    const serverId = searchParams.get("server");
+    const { memberId } = await params;
+    if (!serverId) {
+      return new NextResponse("Server Id missing", { status: 400 });
+    }
+    if (!memberId) {
+      return new NextResponse("Member Id missing", { status: 400 });
+    }
+
+    const server = await prisma.server.update({
+      where: {
+        id: serverId,
+        profileId: profile.id,
+      },
+      data: {
+        members: {
+          update: {
+            where: {
+              id: memberId,
+              profileId: {
+                not: profile.id,
+              },
+            },
+            data: {
+              role,
+            },
+          },
+        },
+      },
+      include: {
+        members: {
+          include: {
+            profile: true,
+          },
+          orderBy: {
+            role: "asc",
+          },
+        },
+      },
+    });
+    console.log("server ", server);
+    return NextResponse.json(server);
+  } catch (error) {
+    console.log(error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
