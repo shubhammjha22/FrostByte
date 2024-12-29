ef12dd43c66407b56bfbd69ede7ac293a98963b7
shubhammjha22 <shubhammjha22@gmail.com>
2024-12-29 21:37:05 +0530

    added patch req for editing channel

--- app/api/channels/[channelId]/route.ts
+++ app/api/channels/[channelId]/route.ts
@@ -56,3 +56,68 @@ export async function DELETE(
     return new NextResponse("Internal Error", { status: 500 });
   }
 }
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { channelId: string } }
+) {
+  try {
+    const { searchParams } = new URL(req.url);
+    const serverId = searchParams.get("serverId");
+    const { name, type } = await req.json();
+    const profile = await currentProfile();
+    if (!profile) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+    if (!serverId) {
+      return new NextResponse("Server ID missing", { status: 400 });
+    }
+
+    const { channelId } = await params;
+    if (!channelId) {
+      return new NextResponse("Channel ID missing", { status: 400 });
+    }
+    console.log("Server id is =", serverId);
+
+    if (name === "general") {
+      return new NextResponse("Name can't be 'general' ", { status: 400 });
+    }
+
+    const server = await prisma.server.update({
+      where: {
+        id: serverId,
+        members: {
+          some: {
+            profileId: profile.id,
+            role: {
+              in: [MemberRole.MODERATOR, MemberRole.ADMIN],
+            },
+          },
+        },
+      },
+      data: {
+        channels: {
+          update: {
+            where: {
+              id: channelId,
+              NOT: {
+                name: "general",
+              },
+            },
+            data: {
+              name,
+              type,
+            },
+          },
+        },
+      },
+    });
+
+    console.log("Server is this - ", server);
+
+    return NextResponse.json(server);
+  } catch (error) {
+    console.log("[CHANNEL_ID_PATCH]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
