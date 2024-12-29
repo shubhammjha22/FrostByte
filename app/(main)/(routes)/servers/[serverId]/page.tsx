67942d8e2bc94f7bfe9b63d96d47bb9d8d353de0
shubhammjha22 <shubhammjha22@gmail.com>
2024-12-29 22:21:28 +0530

    redirecting to general channel when server active

--- app/(main)/(routes)/servers/[serverId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -1,7 +1,47 @@
-export default function ServerIdPage() {
-  return (
-    <>
-      <div>Server ID Page</div>
-    </>
-  );
+import { currentProfile } from "@/lib/current-profile";
+import prisma from "@/lib/db";
+import { redirect } from "next/navigation";
+
+interface ServerIdPageProps {
+  params: {
+    serverId: string;
+  };
+}
+
+export default async function ServerIdPage({ params }: ServerIdPageProps) {
+  const profile = await currentProfile();
+  if (!profile) {
+    redirect("/");
+  }
+
+  const { serverId } = await params;
+
+  const server = await prisma.server.findUnique({
+    where: {
+      id: serverId,
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+    include: {
+      channels: {
+        where: {
+          name: "general",
+        },
+        orderBy: {
+          createdAt: "asc",
+        },
+      },
+    },
+  });
+
+  const initialChannel = server?.channels[0];
+
+  if (initialChannel?.name !== "general") {
+    return null;
+  }
+
+  return redirect(`/servers/${serverId}/channels/${initialChannel?.id}`);
 }
