4a686b665d2735a716107822488daa5321588f6c
shubhammjha22 <shubhammjha22@gmail.com>
2025-01-01 19:45:00 +0530

    chat section 1/n

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,7 +1,49 @@
-export default function ChannelIdPage() {
+import ChatHeader from "@/components/chat/chat-header";
+import { currentProfile } from "@/lib/current-profile";
+import prisma from "@/lib/db";
+import { redirect } from "next/navigation";
+
+interface ChannelIdPageProps {
+  params: {
+    serverId: string;
+    channelId: string;
+  };
+}
+
+export default async function ChannelIdPage({ params }: ChannelIdPageProps) {
+  const { serverId, channelId } = await params;
+  const profile = await currentProfile();
+
+  if (!profile) {
+    redirect("/");
+  }
+
+  const channel = await prisma.channel.findUnique({
+    where: {
+      id: channelId,
+    },
+  });
+
+  const members = await prisma.member.findFirst({
+    where: {
+      serverId,
+      profileId: profile.id,
+    },
+  });
+
+  if (!channel || !members) {
+    redirect("/");
+  }
+
   return (
     <>
-      <div>Channel ID </div>
+      <div className="bg-whie dark:bg-[#313338] flex flex-col h-full">
+        <ChatHeader
+          name={channel.name}
+          serverId={channel.serverId}
+          type="channel"
+        />
+      </div>
     </>
   );
 }
