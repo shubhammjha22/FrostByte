01b3d159aa6421a1f8e9d8d0977a261c3b24904d
shubhammjha22 <shubhammjha22@gmail.com>
2025-01-02 23:41:52 +0530

    added socket types n implement socket 1/n

--- types.ts
+++ types.ts
@@ -1,5 +1,16 @@
 import { Server, Member, Profile } from "@prisma/client";
+import { Server as NetServer, Socket } from "net";
+import { NextApiRequest } from "next";
+import { Server as SocketIO } from "socket.io";
 
 export type ServerWithMembersWithProfiles = Server & {
   members: (Member & { profile: Profile })[];
+};
+
+export type NextApiResponseServerIo = NextApiRequest & {
+  socket: Socket & {
+    server: NetServer & {
+      io?: SocketIO;
+    };
+  };
 };
